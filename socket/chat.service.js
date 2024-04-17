e1cf1f77487f380e853dfc8a899312243a99c2ab
banrana <blazed1347@gmail.com>
2024-04-17 18:11:51 +0700

    3th commit

--- socket/chat.service.js
+++ socket/chat.service.js
@@ -1,15 +1,20 @@
 const chatModel = require('../models/chatModel');
+const userModel = require('../models/userModel');
 
 module.exports.sendMessage = async function(data) {
     try {
         const { roomId, sender, message } = data;
+        const senderInfo = await userModel.findById(sender);
         const newChatMessage = new chatModel({
             roomId,
             sender,
             message
         });
         await newChatMessage.save();
-        return newChatMessage;
+        return {
+            sender: senderInfo ? senderInfo.username : "Unknown",
+            message: message
+        };
     } catch (error) {
         console.error("Lỗi khi gửi tin nhắn:", error);
         throw error;
